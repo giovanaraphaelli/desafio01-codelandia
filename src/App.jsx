7d4abec7418f2b9fb2f21e7563b4c6b41ecb9b4e
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2022-09-01 16:11:08 -0300

    add loading

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Container from './components/Container';
 import Card from './components/Card';
+import Loading from './components/Loading';
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios
       .get('https://inshorts.deta.dev/news?category=technology')
       .then((response) => {
         setPosts(response.data.data);
+        setLoading(false);
       })
       .catch(() => {
         console.log('Deu errado');
@@ -24,16 +27,20 @@ function App() {
       <GlobalStyle />
       <Header />
       <Container>
-        {posts.map((post) => {
-          return (
-            <Card
-              key={post.id}
-              date={post.date}
-              title={post.title}
-              content={post.content}
-            />
-          );
-        })}
+        {loading ? (
+          <Loading />
+        ) : (
+          posts.map((post) => {
+            return (
+              <Card
+                key={post.id}
+                date={post.date}
+                title={post.title}
+                content={post.content}
+              />
+            );
+          })
+        )}
       </Container>
     </>
   );
